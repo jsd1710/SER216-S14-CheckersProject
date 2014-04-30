adb3b9763871389a0c386a5ea14473e0d1ab8d63
Andrew West <drewwest69@Andrews-MacBook-Pro.local>
2014-04-30 11:51:39 -0700

    Fixed: When yellow wins displays Red Wins

--- src/checkers/Checkers.java
+++ src/checkers/Checkers.java
@@ -707,7 +707,7 @@ public class Checkers
             {
                 e.printStackTrace();
             }
-            winner = new GameWin("Red",this.getLocationOnScreen());
+            winner = new GameWin("Yellow",this.getLocationOnScreen());
             
             JButton newGameWinButton = new JButton("New Game");
             newGameWinButton.setActionCommand("Start New Game");
